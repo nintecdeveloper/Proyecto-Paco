31fef7d055fcfb1d210b2e5259dd4102de8e0f65
nintecdeveloper <nintecdeveloper@gmail.com>
2026-03-20 11:18:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
+from werkzeug.middleware.proxy_fix import ProxyFix
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 import io
 import re
